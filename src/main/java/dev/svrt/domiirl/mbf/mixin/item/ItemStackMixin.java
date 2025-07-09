0afda9e549348a34151afeb009d2b7e4f7cb4d17
Dominik <kxmischesdomi@gmail.com>
2025-07-09 20:00:22 +0200

    feat: show max layers for all banners

--- src/main/java/dev/svrt/domiirl/mbf/mixin/item/ItemStackMixin.java
+++ src/main/java/dev/svrt/domiirl/mbf/mixin/item/ItemStackMixin.java
@@ -6,11 +6,13 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponentHolder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BannerItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipDisplay;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,10 +22,12 @@ import java.util.function.Consumer;
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin implements DataComponentHolder, FabricItemStack {
 
+  @Shadow public abstract Item getItem();
+
   @Inject(method = "addDetailsToTooltip", at = @At(value = "FIELD", target = "Lnet/minecraft/core/component/DataComponents;BANNER_PATTERNS:Lnet/minecraft/core/component/DataComponentType;", shift = At.Shift.BEFORE))
   private void addDetailsToTooltip(Item.TooltipContext tooltipContext, TooltipDisplay tooltipDisplay, Player player, TooltipFlag tooltipFlag, Consumer<Component> consumer, CallbackInfo ci) {
 
-    if (has(ModDataComponents.MAX_BANNER_LAYERS)) {
+    if (has(ModDataComponents.MAX_BANNER_LAYERS) || getItem() instanceof BannerItem) {
       int maxLayers = getOrDefault(ModDataComponents.MAX_BANNER_LAYERS, 6);
       consumer.accept(Component.translatable("tooltip.mbf.max_banner_layers", maxLayers).withStyle(ChatFormatting.GRAY));
     }
